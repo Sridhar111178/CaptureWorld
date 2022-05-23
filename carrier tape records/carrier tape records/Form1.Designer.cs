693e07b06278b96b795313f472a51587feaf706d
Sridhar111178 <106045618+Sridhar111178@users.noreply.github.com>
2022-05-23 15:00:13 +0800

    Add files via upload

---
+++ carrier tape records/carrier tape records/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace carrier_tape_records
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Populate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxLotID = new System.Windows.Forms.TextBox();
+            this.textBoxKeyCode = new System.Windows.Forms.TextBox();
+            this.textBoxCarPartNo = new System.Windows.Forms.TextBox();
+            this.textBoxCarBatNo = new System.Windows.Forms.TextBox();
+            this.textBoxCovPartNo = new System.Windows.Forms.TextBox();
+            this.textBoxCovBatNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Populate
+            // 
+            this.Populate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Populate.Location = new System.Drawing.Point(1233, 75);
+            this.Populate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Populate.Name = "Populate";
+            this.Populate.Size = new System.Drawing.Size(161, 69);
+            this.Populate.TabIndex = 0;
+            this.Populate.Text = "Get Data";
+            this.Populate.UseVisualStyleBackColor = true;
+            this.Populate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 208);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1639, 303);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(644, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(245, 29);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(812, 32);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(105, 546);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Export to Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxLotID
+            // 
+            this.textBoxLotID.Location = new System.Drawing.Point(250, 73);
+            this.textBoxLotID.Name = "textBoxLotID";
+            this.textBoxLotID.Size = new System.Drawing.Size(260, 22);
+            this.textBoxLotID.TabIndex = 9;
+            // 
+            // textBoxKeyCode
+            // 
+            this.textBoxKeyCode.Location = new System.Drawing.Point(250, 116);
+            this.textBoxKeyCode.Name = "textBoxKeyCode";
+            this.textBoxKeyCode.Size = new System.Drawing.Size(260, 22);
+            this.textBoxKeyCode.TabIndex = 10;
+            // 
+            // textBoxCarPartNo
+            // 
+            this.textBoxCarPartNo.Location = new System.Drawing.Point(251, 161);
+            this.textBoxCarPartNo.Name = "textBoxCarPartNo";
+            this.textBoxCarPartNo.Size = new System.Drawing.Size(259, 22);
+            this.textBoxCarPartNo.TabIndex = 11;
+            // 
+            // textBoxCarBatNo
+            // 
+            this.textBoxCarBatNo.Location = new System.Drawing.Point(812, 75);
+            this.textBoxCarBatNo.Name = "textBoxCarBatNo";
+            this.textBoxCarBatNo.Size = new System.Drawing.Size(265, 22);
+            this.textBoxCarBatNo.TabIndex = 12;
+            // 
+            // textBoxCovPartNo
+            // 
+            this.textBoxCovPartNo.Location = new System.Drawing.Point(812, 118);
+            this.textBoxCovPartNo.Name = "textBoxCovPartNo";
+            this.textBoxCovPartNo.Size = new System.Drawing.Size(265, 22);
+            this.textBoxCovPartNo.TabIndex = 13;
+            // 
+            // textBoxCovBatNo
+            // 
+            this.textBoxCovBatNo.Location = new System.Drawing.Point(812, 163);
+            this.textBoxCovBatNo.Name = "textBoxCovBatNo";
+            this.textBoxCovBatNo.Size = new System.Drawing.Size(265, 22);
+            this.textBoxCovBatNo.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Lot ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Key Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Carrier Tape Part No";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(645, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Carrier Tape Batch No";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(645, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 17);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Cover Tape Part No";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(645, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 17);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Cover Tape Batch No";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1653, 608);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCovBatNo);
+            this.Controls.Add(this.textBoxCovPartNo);
+            this.Controls.Add(this.textBoxCarBatNo);
+            this.Controls.Add(this.textBoxCarPartNo);
+            this.Controls.Add(this.textBoxKeyCode);
+            this.Controls.Add(this.textBoxLotID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Populate);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "T&R Materials Record";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Populate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxLotID;
+        private System.Windows.Forms.TextBox textBoxKeyCode;
+        private System.Windows.Forms.TextBox textBoxCarPartNo;
+        private System.Windows.Forms.TextBox textBoxCarBatNo;
+        private System.Windows.Forms.TextBox textBoxCovPartNo;
+        private System.Windows.Forms.TextBox textBoxCovBatNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ carrier tape records/carrier tape records/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+
+namespace carrier_tape_records
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Persist Security Info=False;User ID=EOLUSER1;Password=EOLUSER02;Initial Catalog=UTAC_EOL;Server=168.232.225.161\\EOLDBSVR02");
+            cmd = new SqlCommand("select * from JSDOWNLOAD_SUMMARY", con);
+         
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd.Parameters.Clear();
+            //command.Parameters.Add("ArrivedDate", SqlDbType.DateTime).Value = ArrivedDate;
+            //command.Parameters.Add("ReturnDate", SqlDbType.DateTime).Value = ReturnDate;
+            //cmd.Parameters.Add("@date1", SqlDbType.DateTime).Value = dateTimePicker1.Value.ToString();
+            //cmd.Parameters.Add("@date2", SqlDbType.DateTime).Value = dateTimePicker2.Value.ToString();
+            cmd.Parameters.AddWithValue("@date1", dateTimePicker1.Value.ToString());
+            cmd.Parameters.AddWithValue("@date2", dateTimePicker2.Value.ToString());            
+            cmd.Parameters.AddWithValue("@LotID", textBoxLotID.Text.ToString());
+            cmd.Parameters.AddWithValue("@KeyCode", textBoxKeyCode.Text.ToString());
+            cmd.Parameters.AddWithValue("@CarPartNo", textBoxCarPartNo.Text.ToString());
+            cmd.Parameters.AddWithValue("@CarBatNo", textBoxCarBatNo.Text.ToString());
+            cmd.Parameters.AddWithValue("@CovPartNo", textBoxCovPartNo.Text.ToString());
+            cmd.Parameters.AddWithValue("@CovBatNo", textBoxCovBatNo.Text.ToString());
+
+            con.Open();
+
+            if(string.IsNullOrEmpty(textBoxLotID.Text) && string.IsNullOrEmpty(textBoxKeyCode.Text) && string.IsNullOrEmpty(textBoxCarPartNo.Text) && string.IsNullOrEmpty(textBoxCarBatNo.Text) && string.IsNullOrEmpty(textBoxCovPartNo.Text) && string.IsNullOrEmpty(textBoxCovBatNo.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+           "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+           "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+           "and RequestStatus = 'Success' and LotID like 'U%' ";
+            }
+
+            else if(!string.IsNullOrEmpty(textBoxLotID.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and LotId = @LotID ";
+            }
+            else if (!string.IsNullOrEmpty(textBoxKeyCode.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and KeyCode = @KeyCode ";
+            }
+            else if (!string.IsNullOrEmpty(textBoxCarPartNo.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and CarrierTapePartNo1 = @CarPartNo or CarrierTapePartNo2 = @CarPartNo ";
+            }
+            else if (!string.IsNullOrEmpty(textBoxCarBatNo.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and CarrierTapeBatch1 = @CarBatNo or CarrierTapeBatch2 = @CarBatNo ";
+            }
+            else if (!string.IsNullOrEmpty(textBoxCovPartNo.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and CoverTapePartNo1 = @CovPartNo or CoverTapePartNo2 = @CovPartNo ";
+            }
+            else if (!string.IsNullOrEmpty(textBoxCovBatNo.Text))
+            {
+                cmd.CommandText = "select MachineID,UserID,LotId,KeyCode,RequestDate,CarrierTapePartNo1,CarrierTapeBatch1,CoverTapePartNo1," +
+             "CoverTapeBatch1,CarrierTapePartNo2,CarrierTapeBatch2,CoverTapePartNo2,CoverTapeBatch2," +
+             "RequestStatus from JSDOWNLOAD_SUMMARY where RequestDate between @date1 And @date2 and PackType = 'Reel' " +
+             "and RequestStatus = 'Success' and LotID like 'U%' and CoverTapeBatch1 = @CovBatNo or CoverTapeBatch2 = @CovBatNo ";
+            }
+
+
+
+            //cmd.CommandText = "select * from JSDOWNLOAD_SUMMARY where lotId = 'UI38D362.1' ";
+            SqlDataAdapter adaptor = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adaptor.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+       
+        }
+  
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            app.Visible = true;
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, j + 1] = "";
+                    }
+                }
+            }
+
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    dataGridView1.DataSource = null;
+        //    dataGridView1.Rows.Clear();
+        //}
+    }
+}
